5d87e96849e8a51dcb6823c2205d13c90a998b7f
techykishan <tauleshwarthakur@gmail.com>
2022-10-14 00:05:22 +0530

    2nd pr form tauleshwar

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
   return (
-   <div className="hompeage">
-    <h1 className="title">Technica</h1>
+   <div className="hompeage text-center">
+    <h1 className="title text-3xl font-extrabold  ">Technica</h1>
+    <p className='text-lg font-semibold'>the official coding club at Satyam Institute of Engineering and Technology</p>
    </div>
   )
 }
